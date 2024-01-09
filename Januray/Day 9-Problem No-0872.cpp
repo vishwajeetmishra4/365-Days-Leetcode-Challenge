8e6265005b9f0d96b081d9131cc4e3526db9361d
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-01-09 19:36:51 +0530

    Add files via upload

---
+++ Januray/Day 9-Problem No-0872.cpp
@@ -0,0 +1,28 @@
+//Day 9 of 365 Days Challenge.
+
+//Problem number 0872-Leaf Similar Trees.
+
+// Code Goes from Here
+
+class Solution {
+ public:
+  bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+    vector<int> leaves1;
+    vector<int> leaves2;
+    dfs(root1, leaves1);
+    dfs(root2, leaves2);
+    return leaves1 == leaves2;
+  }
+
+  void dfs(TreeNode* root, vector<int>& leaves) {
+    if (root == nullptr)
+      return;
+    if (root->left == nullptr && root->right == nullptr) {
+      leaves.push_back(root->val);
+      return;
+    }
+
+    dfs(root->left, leaves);
+    dfs(root->right, leaves);
+  }
+};
