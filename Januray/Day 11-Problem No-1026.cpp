1dce2ce95cfea2699b9d5066ff4384d876a6ff94
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-01-11 15:02:56 +0530

    Add files via upload

---
+++ Januray/Day 11-Problem No-1026.cpp
@@ -0,0 +1,25 @@
+//Day 11 of 365 Days Challenge.
+
+//Problem number 1026-Maximum Difference Between Node and Ancestor.
+
+// Code Goes from Here
+
+
+class Solution {
+ public:
+  int maxAncestorDiff(TreeNode* root) {
+    return maxAncestorDiff(root, root->val, root->val);
+  }
+
+ private:
+  // Returns |the maximum - the minimum| of the tree.
+  int maxAncestorDiff(TreeNode* root, int mini, int maxi) {
+    if (root == nullptr)
+      return 0;
+    mini = min(mini, root->val);
+    maxi = max(maxi, root->val);
+    const int l = maxAncestorDiff(root->left, mini, maxi);
+    const int r = maxAncestorDiff(root->right, mini, maxi);
+    return max({maxi - mini, l, r});
+  }
+};
