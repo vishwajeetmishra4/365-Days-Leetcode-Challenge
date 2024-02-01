0eb0e5bb256aa25e47333461339f4d6b5a35911a
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-02-01 12:20:07 +0530

    Day 1 Problem No-2966.cpp

---
+++ February/Day 1 Problem No-2966.cpp
@@ -0,0 +1,22 @@
+// Day 1 of 364 Days Coding Challange.
+
+// Problem number-2966-Divide Array into Arrays with max Difference.
+
+// Code Goes From Here.
+
+class Solution {
+ public:
+  vector<vector<int>> divideArray(vector<int>& nums, int k) {
+    vector<vector<int>> ans;
+
+    ranges::sort(nums);
+
+    for (int i = 2; i < nums.size(); i += 3) {
+      if (nums[i] - nums[i - 2] > k)
+        return {};
+      ans.push_back({nums[i - 2], nums[i - 1], nums[i]});
+    }
+
+    return ans;
+  }
+};
