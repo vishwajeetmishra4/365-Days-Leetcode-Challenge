7738d412a71df5119396ba6d4c4db16c55a8138b
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-02-03 09:23:13 +0530

    Day 3 Problem no-1043.cpp

---
+++ February/Day 3 Problem no-1043.cpp
@@ -0,0 +1,23 @@
+// Day 3 of 365 Days Coding Challange.
+
+// Problem number 1043-Partition Array for Maximum Sum.
+
+// Code Goes From Here.
+
+class Solution {
+ public:
+  int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    const int n = arr.size();
+    vector<int> dp(n + 1);
+
+    for (int i = 1; i <= n; ++i) {
+      int maxi = INT_MIN;
+      for (int j = 1; j <= min(i, k); ++j) {
+        maxi = max(maxi, arr[i - j]);
+        dp[i] = max(dp[i], dp[i - j] + maxi * j);
+      }
+    }
+
+    return dp[n];
+  }
+};
