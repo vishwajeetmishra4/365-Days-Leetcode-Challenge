839e6f73252a2601c5a7ffe5e77b9558e27f2384
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-02-07 09:26:06 +0530

    Day 7 Problem no-0451.cpp

---
+++ February/Day 7 Problem no-0451.cpp
@@ -0,0 +1,31 @@
+// Day 7 of 365 Days Coding Challange.(2nd Month)
+
+// Problem number-451-Sort Characters by Frequency.
+
+// Code Goes From Here.
+
+class Solution {
+ public:
+  string frequencySort(string s) {
+    const int n = s.length();
+    string ans;
+    vector<int> count(128);
+    // buckets[i] := characters that appear i times in s
+    vector<vector<char>> buckets(n + 1);
+
+    for (const char c : s)
+      ++count[c];
+
+    for (int i = 0; i < 128; ++i) {
+      const int freq = count[i];
+      if (freq > 0)
+        buckets[freq].push_back((char)i);
+    }
+
+    for (int freq = n; freq > 0; --freq)
+      for (const char c : buckets[freq])
+        ans += string(freq, c);
+
+    return ans;
+  }
+};
