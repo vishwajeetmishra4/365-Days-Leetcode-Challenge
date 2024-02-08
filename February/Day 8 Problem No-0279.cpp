f2c67555fa5bbce66beb62865aea7c2f3be8043a
Vishwajeet Mishra <135427511+vishwajeetmishra4@users.noreply.github.com>
2024-02-08 09:11:42 +0530

    Day 8 Problem No-0279.cpp

---
+++ February/Day 8 Problem No-0279.cpp
@@ -0,0 +1,20 @@
+// Day 8 of 365 Days Coding Challange.(2nd Month)
+
+// Problem number-279-Perfect Squares.
+
+// Code Goes From Here.
+
+class Solution {
+ public:
+  int numSquares(int n) {
+    vector<int> dp(n + 1, n);  // 1^2 x n
+    dp[0] = 0;                 // no way
+    dp[1] = 1;                 // 1^2
+
+    for (int i = 2; i <= n; ++i)
+      for (int j = 1; j * j <= i; ++j)
+        dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+    return dp[n];
+  }
+};
